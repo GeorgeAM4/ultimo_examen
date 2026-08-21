2bd1ef794bcbba6cd85025537db128ed7917dee0
GeorgeAM4 <www.apazajorgem1@gmail.com>
2026-08-20 23:40:10 -0400

    Update ejercicio_cuatro.py

--- ejercicio_cuatro.py
+++ ejercicio_cuatro.py
@@ -1,8 +1,20 @@
  #EJERCICIO 4
-contrasena = input("Ingresa una contraseña: ").strip()
-while contrasena == "12345":
-    contrasena = input("Contraseña insegura, ingresa otra: ").strip()
-if contrasena != "12345":
-    print("Acceso concedido")
-else:
-    print("Acceso denegado")
+for curso in range(1, 5):
+    asistieron = 0
+    faltaron = 0
+
+    print("CURSO", curso)
+
+    for estudiante in range(1, 7):
+        asistencia = int(input(
+            f"Estudiante {estudiante} (1 = asistió, 0 = faltó): "
+        ))
+
+        if asistencia == 1:
+            asistieron = asistieron + 1
+        else:
+            faltaron = faltaron + 1
+
+    print("Asistieron:", asistieron)
+    print("Faltaron:", faltaron)
+    print()
